85ffd20c36cedf999f34c28b6cc10d423ba8043b
knaseem <knaseem22@gmail.com>
2016-07-21 17:52:01 -0400

    Initial Commit

---
+++ SnapLister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapLister
+//
+//  Created by Khalid Naseem on 21/07/2016.
+//  Copyright © 2016 Khalid Naseem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
